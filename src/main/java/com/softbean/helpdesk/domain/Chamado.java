ad8cce4b5f6bf8cfd83972ff9f4054d5f8d634ab
victorps010391 <victorps91@gmail.com>
2021-08-11 22:42:11 -0300

    anotações para classe de dominio

--- src/main/java/com/softbean/helpdesk/domain/Chamado.java
+++ src/main/java/com/softbean/helpdesk/domain/Chamado.java
@@ -1,22 +1,46 @@
 package com.softbean.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.softbean.helpdesk.domain.enums.Prioridade;
 import com.softbean.helpdesk.domain.enums.Status;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbertura = LocalDate.now();	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+		
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
 	public Chamado() {
 		super();	
 	}
--- src/main/java/com/softbean/helpdesk/domain/Tecnico.java
+++ src/main/java/com/softbean/helpdesk/domain/Tecnico.java
@@ -3,16 +3,26 @@ package com.softbean.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.softbean.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super(); 
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
