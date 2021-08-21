c0d6fad4483a034b61834fe997313302ae7d5641
victorps010391 <victorps91@gmail.com>
2021-08-20 22:12:08 -0300

    autorizacao com JWT

--- src/main/java/com/softbean/helpdesk/config/DevConfig.java
+++ src/main/java/com/softbean/helpdesk/config/DevConfig.java
@@ -20,7 +20,7 @@ public class DevConfig {
 
 	@Bean
 	public boolean instanciaDB() {
-		
+
 		if (value.equals("create")) {
 			this.dbService.instanciaDB();
 		}
--- src/main/java/com/softbean/helpdesk/security/UserSS.java
+++ src/main/java/com/softbean/helpdesk/security/UserSS.java
@@ -12,20 +12,19 @@ import com.softbean.helpdesk.domain.enums.Perfil;
 
 public class UserSS implements UserDetails {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String email;
 	private String senha;
-	private Collection<? extends GrantedAuthority> authorities; 
-	
-	
+	private Collection<? extends GrantedAuthority> authorities;
 
 	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
 		super();
 		this.id = id;
 		this.email = email;
 		this.senha = senha;
-		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toSet());
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao()))
+				.collect(Collectors.toSet());
 	}
 
 	public Integer getId() {
@@ -33,40 +32,38 @@ public class UserSS implements UserDetails {
 	}
 
 	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {		
+	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return authorities;
 	}
 
 	@Override
-	public String getPassword() {		
+	public String getPassword() {
 		return senha;
 	}
 
 	@Override
-	public String getUsername() {		
+	public String getUsername() {
 		return email;
 	}
 
 	@Override
-	public boolean isAccountNonExpired() {		
+	public boolean isAccountNonExpired() {
 		return true;
 	}
 
 	@Override
-	public boolean isAccountNonLocked() {		
+	public boolean isAccountNonLocked() {
 		return true;
 	}
 
 	@Override
-	public boolean isCredentialsNonExpired() {		
+	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
 	@Override
-	public boolean isEnabled() {		
+	public boolean isEnabled() {
 		return true;
 	}
-	
-	
 
 }
